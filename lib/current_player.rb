d14ce891868a8bc545d0067cd32354d97231543e
Victor Torres <torres.victor136@gmail.com>
2020-07-10 23:37:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,15 +2,15 @@ def turn_count(board)
   turn = 0
   board.each do |space|
     if space == "X" || space == "O"
-      turns += 1
+      turn += 1
     end
   end
   return turn
 end
 
 def current_player(board)
-  T = turn_count(board)
-  if T % 2 == 0
+  turn = turn_count(board)
+  if turn % 2 == 0
     return "X"
   else
     return "O"
